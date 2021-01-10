8452ccabd7a9cc900d10ac08b0d657120c0746f6
NeoDragon <mcvickerstephen@gmail.com>
2021-01-10 23:18:25 +0000

    First commit WIP

---
+++ README.md
@@ -0,0 +1,4 @@
+# friend-list
+
+This is still a WIP.
+README will be updated after project is completed.
---
+++ src/components/Navigation/HamburgerMenu.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  .hamburger(@click="isOpen = !isOpen")
+    div(:data-open="isOpen")
+    div(:data-open="isOpen")
+    div(:data-open="isOpen")
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.hamburger {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+
+  display: flex;
+  flex-flow: column nowrap;
+  height: 2rem;
+  justify-content: space-around;
+  transition: all 0.2s;
+  width: 2rem;
+
+  [data-open="true"] {
+    &:nth-child(1) {
+      transform: rotate(45deg);
+    }
+
+    &:nth-child(2) {
+      opacity: 0;
+    }
+
+    &:nth-child(3) {
+      transform: rotate(-45deg);
+    }
+  }
+
+  div {
+    background-color: white;
+    border-radius: 50vh;
+    height: 0.25rem;
+
+    /* animation */
+    transform-origin: 1px;
+    transition: all 0.2s;
+    width: 2rem;
+  }
+}
+</style>
